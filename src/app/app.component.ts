121b4218fcb086785fc860366d29be33289a3a68
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-10 11:22:01 +0700

    Using ViewContainerRef & TemplateRef & Asterisk structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
 import { CommonModule } from '@angular/common';
+import { AuthDirective } from './auth/auth.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, CommonModule],
+  imports: [AuthComponent, LearningResourcesComponent, CommonModule, AuthDirective],
 })
 export class AppComponent {
   private authService = inject(AuthService);
